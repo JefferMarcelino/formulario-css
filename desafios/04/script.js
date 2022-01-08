bea0914b5880528069030f825581409af03a7dcf
Jeffer Marcelino <jeffermarcelinodev@gmail.com>
2022-01-08 20:27:13 +0200

    Resolvendo os bugs no HTML

---
+++ desafios/04/script.js
@@ -0,0 +1,9 @@
+const btnLogin = document.querySelector(".btn-login");
+const form = document.querySelector("form");
+
+btnLogin.addEventListener("click", event => {
+  event.preventDefault();
+  form.classList.add("form-hide");
+});
+
+form.addEventListener()
